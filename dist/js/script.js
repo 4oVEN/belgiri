8e62b7128195ffe43ae3bf9b1aa0f44c843aeae1
Nikita <ndemidovn@gmail.com>
2022-11-29 15:35:10 +0300

    new photo

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,18 @@
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu');
+  menu = document.querySelector('.menu');
 
-      function toggleMenu() {
-        hamburger.classList.toggle('active');
-        menu.classList.toggle('active');
-      }
-      hamburger.addEventListener('click', toggleMenu);
-      menu.addEventListener('click', toggleMenu);
-      
-
+function toggleMenu() {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+}
+hamburger.addEventListener('click', toggleMenu);
+menu.addEventListener('click', toggleMenu);
 
 
 
-$(document).ready(function(){
+
+
+$(document).ready(function () {
   $('.carousel__inner').slick({
     speed: 1200,
     autoplay: true,
@@ -28,25 +28,25 @@ $(document).ready(function(){
           arrows: false
         }
       }
-      
+
     ]
   });
- 
+
   // Modal
-  $('[data-modal=registration]').on('click', function(){
+  $('[data-modal=registration]').on('click', function () {
     $('.overlay, #application').fadeIn('slow');
   });
-  $('.modal__close').on('click', function(){
+  $('.modal__close').on('click', function () {
     $('.overlay, #application, #thanks').fadeOut('slow');
   });
 
-  $('.feed-form').submit(function(e){
+  $('.feed-form').submit(function (e) {
     e.preventDefault();
     $.ajax({
       type: 'POST',
       url: 'mailer/smart.php',
       data: $(this).serialize()
-    }).done(function(){
+    }).done(function () {
       $(this).find('input').val('');
       $('#application').fadeOut();
       $('.overlay, #thanks').fadeIn('slow');
@@ -56,54 +56,59 @@ $(document).ready(function(){
     return false;
   });
 
-  $('.contacts__form').submit(function(e){
+  $('.contacts__form').submit(function (e) {
     e.preventDefault();
     $.ajax({
       type: 'POST',
       url: 'mailer/smart.php',
       data: $(this).serialize()
-    }).done(function(){
+    }).done(function () {
       $(this).find('input', 'textarea').val('');
 
       $('form').trigger('reset');
     })
     return false;
   });
-    // Smooth scroll and pageup
+  // Smooth scroll and pageup
 
-    $(window).scroll(function(){
-      if ($(this).scrollTop() > 1600){
-        $('.pageup').fadeIn();
-      } else {
-        $('.pageup').fadeOut();
-      }
-    });
-
-    const runText = document.querySelector('#text');
-
-    const line = 'Напишите сообщение здесь.....';
-    const speed = 150;
-
-    // for (let i = 0; i <= line.length; i++){
-    //   runText.value = line.substring(0, i);
-    // }
-
-    let i = 0;
-    function runLine(){
-      if(i++ < line.length){
-        runText.value = line.substring(0, i);
-      }
-      else {
-        runText.value = '';
-        i = 0;
-      }
-      done = setTimeout(runLine, speed);
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
     }
-    runLine();
+  });
 
-    runText.onfocus = function(){
-      clearTimeout(done); 
-      this.value = '';
+  const runText = document.querySelector('#text');
+
+  const line = 'Напишите сообщение здесь.....';
+  const speed = 150;
+
+  // for (let i = 0; i <= line.length; i++){
+  //   runText.value = line.substring(0, i);
+  // }
+
+  let i = 0;
+  function runLine() {
+    if (i++ < line.length) {
+      runText.value = line.substring(0, i);
     }
-    
+    else {
+      runText.value = '';
+      i = 0;
+    }
+    done = setTimeout(runLine, speed);
+  }
+  runLine();
+
+  runText.onfocus = function () {
+    clearTimeout(done);
+    this.value = '';
+  }
+
 });
+
+
+
+
+        
